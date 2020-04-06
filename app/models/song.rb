bea1b8e49d12d948a012037f4a27cd1d54f770dd
Beekasha <brian.j.kleiner@gmail.com>
2020-04-06 01:56:41 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,30 @@
+class Song < ApplicationRecord
+    validates :title, presence: true
+    validates :title, uniqueness: {
+        scope: %i[release_year artist_name],
+        message: "should happen once per year" 
+    }
+ 
+
+    validates :released, inclusion: { in: [true, false] }
+    validates :artist_name, presence: true
+
+    
+    with_options if: :released? do |song|
+        song.validates :release_year, presence: true
+        song.validates :release_year, numericality: {
+        less_than_or_equal_to: Date.today.year
+        }
+    end
+
+
+    def released?
+        released
+    end
+end
+
+# %i[ test ]
+# # => [:test]
+# str = "other"
+# %I[ test_#{str} ]
+# # => [:test_other] 
